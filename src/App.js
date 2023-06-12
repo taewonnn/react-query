074e2cd10d5e3b5a6f1d2bece8e56390ef782096
TaeWon Park <thisis_tw94@naver.com>
2023-06-12 21:11:39 +0900

    react-query devtools

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import MainProducts from './components/MainProducts';
 import { useMutation, useQueryClient, QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 
 
 // Create a client
@@ -11,6 +12,7 @@ export default function App() {
   return (
     <QueryClientProvider client={queryClient}>
       <MainProducts />
+      <ReactQueryDevtools initialIsOpen={true} />
     </QueryClientProvider>
   );
 }
